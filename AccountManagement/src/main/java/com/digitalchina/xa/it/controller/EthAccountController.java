6702e4ec59c878a0d0e9acc4f3ee9d3f18eac8f6
superman7 <pudge_fan@163.com>
2018-08-16 17:11:54 +0800

    修改密码错误时的提示。

--- AccountManagement/src/main/java/com/digitalchina/xa/it/controller/EthAccountController.java
+++ AccountManagement/src/main/java/com/digitalchina/xa/it/controller/EthAccountController.java
@@ -59,8 +59,8 @@ public class EthAccountController {
 	private MnemonicService mnemonicService;
 	private static String[] ip = {"http://10.7.10.124:8545","http://10.7.10.125:8545","http://10.0.5.217:8545","http://10.0.5.218:8545","http://10.0.5.219:8545"};
 	private static String address = "0x024a3c0d945739237eedf78c80c6ae5daf22c010";
-//	private static String tempFilePath = "C://temp/";
-	private static String tempFilePath = "/eth/javaServer/wallet/temp/";
+	private static String tempFilePath = "C://temp/";
+//	private static String tempFilePath = "/eth/javaServer/wallet/temp/";
 //	private static String keystoreName = "keystore.json";
 	
 //	@ResponseBody
@@ -202,6 +202,7 @@ public class EthAccountController {
 				web3jList.add(Web3j.build(new HttpService(ip[i])));
 			}
 			File keystoreFile = keystoreToFile(keystore, account + ".json");
+			System.out.println("开始解锁。。。");
 			Credentials credentials = WalletUtils.loadCredentials(password, keystoreFile);
 			System.out.println("解锁成功。。。");
 			keystoreFile.delete();
@@ -228,7 +229,7 @@ public class EthAccountController {
 			modelMap.put("success", true);
 			modelMap.put("transactionHash", transactionHash);
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("解锁失败。。。");
 			if(e.getMessage().contains("Invalid")) {
 				modelMap.put("success", false);
 				modelMap.put("errMsg", "invalidPassword");
