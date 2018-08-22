9307ed060d365d08d188d0c4b9bdeab4264cbbca
superman7 <pudge_fan@163.com>
2018-08-22 15:48:06 +0800

    修复临时文件路径不对bug。

--- AccountManagement/src/main/java/com/digitalchina/xa/it/controller/EthAccountController.java
+++ AccountManagement/src/main/java/com/digitalchina/xa/it/controller/EthAccountController.java
@@ -65,10 +65,9 @@ public class EthAccountController {
 	private WalletTransactionService walletTransactionService;
 	
 	private static String[] ip = {"http://10.7.10.124:8545","http://10.7.10.125:8545","http://10.0.5.217:8545","http://10.0.5.218:8545","http://10.0.5.219:8545"};
-	private static String address = "0x024a3c0d945739237eedf78c80c6ae5daf22c010";
-	private static String tempFilePath = "C://temp/";
-//	private static String tempFilePath = "/eth/javaServer/wallet/temp/";
-	private static String keystoreName = "keystore.json";
+//	private static String address = "0x024a3c0d945739237eedf78c80c6ae5daf22c010";
+//	private static String tempFilePath = "C://temp/";
+	private static String tempFilePath = "/eth/javaServer/wallet/temp/";
 	
 //	@ResponseBody
 //	@GetMapping("/refreshAllUsersBalance")
@@ -175,7 +174,7 @@ public class EthAccountController {
 			for(int i = 0; i < ip.length; i++) {
 				web3jList.add(Web3j.build(new HttpService(ip[i])));
 			}
-			File keystoreFile = keystoreToFile(keystore, keystoreName);//defaultAcc + ".json"
+			File keystoreFile = keystoreToFile(keystore, defaultAcc + ".json");//
 			Credentials credentials = WalletUtils.loadCredentials("mini0823", keystoreFile);
 			System.out.println("解锁成功。。。");
 			keystoreFile.delete();
