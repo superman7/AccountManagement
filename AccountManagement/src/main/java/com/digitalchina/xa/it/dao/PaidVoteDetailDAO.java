1bc1a656f6039e15eba7ea7637ccbe83fda79ce1
superman7 <pudge_fan@163.com>
2018-09-29 22:43:41 +0800

    封装paidVoteDetailDAO层方法，添加CRU方法。

---
+++ AccountManagement/src/main/java/com/digitalchina/xa/it/dao/PaidVoteDetailDAO.java
@@ -0,0 +1,23 @@
+package com.digitalchina.xa.it.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.digitalchina.xa.it.model.PaidVoteDetailDomain;
+
+public interface PaidVoteDetailDAO {
+	int insertPaidVoteDetail(PaidVoteDetailDomain paidVoteDetailDomain);
+	
+	List<PaidVoteDetailDomain> selectPaidVoteDetailByTopicId(@Param("topicId")Integer topicId);
+	
+	List<PaidVoteDetailDomain> selectPaidVoteDetailByVoteAddress(@Param("voteAddree")String voteAddree);
+	
+	List<PaidVoteDetailDomain> selectPaidVoteDetailByBeVotedAddress(@Param("beVotedAddress")String beVotedAddress);
+	
+	int selectTotalValueByBeVotedAddress(@Param("beVotedAddress")String beVotedAddress);
+	
+	void updateTransactionHash(@Param("id")Integer id, @Param("transactionHash")String transactionHash);
+	
+	List<String> selectUnfinishedTransaction();
+}
