f050554bf8de35e15041db3f879ffc3a4f2eb79b
liz <tinyfourxupt@163.com>
2018-12-06 16:30:55 +0800

    添加与交易信息总表system_transactiondetail的交互，及插入基本信息的方法。

---
+++ AccountManagement/src/main/java/com/digitalchina/xa/it/dao/SystemTransactionDetailDAO.java
@@ -0,0 +1,7 @@
+package com.digitalchina.xa.it.dao;
+
+import com.digitalchina.xa.it.model.SystemTransactionDetailDomain;
+
+public interface SystemTransactionDetailDAO {
+	int insertBaseInfo(SystemTransactionDetailDomain systemTransactionDetailDomain);
+}
