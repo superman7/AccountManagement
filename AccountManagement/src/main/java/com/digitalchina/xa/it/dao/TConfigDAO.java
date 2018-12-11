1ccb5c80a4489a1929092330cb850e9424001d74
liz <tinyfourxupt@163.com>
2018-12-11 15:22:06 +0800

    添加TConfigUtils和DecryptAndDecodeUtils。

---
+++ AccountManagement/src/main/java/com/digitalchina/xa/it/dao/TConfigDAO.java
@@ -0,0 +1,19 @@
+package com.digitalchina.xa.it.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.digitalchina.xa.it.model.TConfigDomain;
+
+public interface TConfigDAO {
+	List<String> selectIpArr();
+	
+	int UpdateEthNodesStatus(@Param("cfgValue")String cfgValue, @Param("cfgStatus")Boolean cfgStatus);
+	
+	String selectValueByKey(@Param("cfgKey")String cfgKey);
+	
+	List<TConfigDomain> selectEthNodesInfo();
+	
+	List<TConfigDomain> selectContractInfo();
+}
---
+++ AccountManagement/src/main/java/com/digitalchina/xa/it/service/impl/TConfigServiceImpl.java
@@ -0,0 +1,54 @@
+package com.digitalchina.xa.it.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.digitalchina.xa.it.dao.TConfigDAO;
+import com.digitalchina.xa.it.model.TConfigDomain;
+import com.digitalchina.xa.it.service.TConfigService;
+
+@Service(value = "tConfigService")
+public class TConfigServiceImpl implements TConfigService {
+	@Autowired
+    private TConfigDAO tConfigDAO;
+	
+	@Override
+	public List<String> selectIpArr() {
+		return tConfigDAO.selectIpArr();
+	}
+
+	@Override
+	public Boolean UpdateEthNodesStatus(String cfgValue, Boolean cfgStatus) {
+		if(cfgValue != null && cfgValue != "" && cfgStatus != null) {
+			try {
+				int effectedNumber = tConfigDAO.UpdateEthNodesStatus(cfgValue, cfgStatus);
+				if(effectedNumber > 0) {
+					return true;
+				} else {
+					throw new RuntimeException("更新以太坊节点状态失败");
+				}
+			} catch (Exception e) {
+				throw new RuntimeException("更新以太坊节点状态失败" + e.getMessage());
+			}
+		} else {
+			throw new RuntimeException("更新以太坊节点状态cfgValue,cfgStatus不能为空");
+		}
+	}
+
+	@Override
+	public String selectValueByKey(String cfgKey) {
+		return tConfigDAO.selectValueByKey(cfgKey);
+	}
+
+	@Override
+	public List<TConfigDomain> selectEthNodesInfo() {
+		return tConfigDAO.selectEthNodesInfo();
+	}
+
+	@Override
+	public List<TConfigDomain> selectContractInfo() {
+		return tConfigDAO.selectContractInfo();
+	}
+}
